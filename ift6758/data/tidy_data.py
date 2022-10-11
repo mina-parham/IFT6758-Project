54e0d4961053d477622657ccdacf981d6938f843
mina parham <minaparham@minas-MacBook-Air.local>
2022-10-10 23:11:09 -0400

    Qunestion 3 tested

--- ift6758/data/tidy_data.py
+++ ift6758/data/tidy_data.py
@@ -1,8 +1,5 @@
 import pandas as pd
 import numpy as np
-
-
-
 def tidy(df) -> pd.DataFrame:
     """
     Clean the json files downloaded with get_data.py function
@@ -25,10 +22,10 @@ def tidy(df) -> pd.DataFrame:
     """
     event_idx, period_time, period, game_id, team_away_name, team_home_name, is_goal, coordinate_x,\
      coordinate_y, shot_type, strength, shooter_name, goalie_name, empty_net, team_name = ([] for i in range(15))
-    for i in df:
-        allplays_data = df[:,i]['liveData']['plays']['allplays']
+    for i in range(df.shape[1]):
+        allplays_data = df.iloc[:,i]['liveData']['plays']['allPlays']
         for j in range(len(allplays_data)):
-            if(j['result']['eventTypeId'] == "SHOT" or "GOAL"):
+            if(allplays_data[j]['result']['eventTypeId'] == ("SHOT" or "GOAL")):
                 period.append(allplays_data[j]['about']['period'])
                 period_time.append(allplays_data[j]['about']['periodTime'])
                 game_id.append(df.iloc[:,i].name)
@@ -41,32 +38,22 @@ def tidy(df) -> pd.DataFrame:
                 coordinate_y.append(allplays_data[j]['coordinates']['y'] if  'y' in allplays_data[j]['coordinates'] else np.nan)
                 shot_type.append(allplays_data[j]['result']['secondaryType'] if 'secondaryType' in allplays_data[j]['result'] else np.nan)
                 strength.append(allplays_data[j]['result']['strength']['code'] if 'strength' in allplays_data[j]['result'] else np.nan)
-                if allplays_data[j]['players']['playerType'] == "Shooter" or 'Scorer':
-                    shooter_name.append(allplays_data[j]['players']['fullName'])
-                elif allplays_data[j]['players']['playerType']=="Goalie":
-                    goalie_name.append(allplays_data[j]['players']['fullName'])
+                if (allplays_data[j]['players'][z]['playerType'] == ("Shooter" or 'Scorer') for z in range(len(allplays_data[j]['players']))):
+                    shooter_name.append([allplays_data[j]['players'][z]['player']['fullName'] for z in range(len(allplays_data[j]['players']))][0])
+                if (allplays_data[j]['players'][z]['playerType']=="Goalie" for z in range(len(allplays_data[j]['players']))):
+                    goalie_name.append([allplays_data[j]['players'][z]['player']['fullName'] for z in range(len(allplays_data[j]['players']))][0])
                 empty_net.append(True if 'emptyNet' in allplays_data[j]['result'] and allplays_data[j]['result']['emptyNet']==True else False)
 
-    
-    assert(all(len(lists) == len(game_id) for lists in [event_idx, period_time, period, game_id, team_away_name, team_home_name, is_goal, coordinate_x,\
+
+
+    assert(all(len(lists) == len(game_id) for lists in [event_idx, period_time, period, team_away_name, team_home_name, is_goal, coordinate_x,\
      coordinate_y, shot_type, strength, shooter_name, goalie_name, empty_net, team_name]) )
 
-    df2 = pd.DataFrame(np.column_stack([event_idx, period_time, period, game_id, team_away_name, team_home_name, is_goal, coordinate_x,\
+    df_main = pd.DataFrame(np.column_stack([event_idx, period_time, period, game_id, team_away_name, team_home_name, is_goal, coordinate_x,\
      coordinate_y, shot_type, strength, shooter_name, goalie_name, empty_net, team_name]),
                        columns=['event_idx', 'period_time', 'period', 'game_id', 'team_away_name', 'team_home_name','is_goal', 'coordinate_x',
                         'coordinate_y', 'shot_type', 'strength', 'shooter_name','goalie_name', 'empty_net', 'team_name'])
 
-    return df2
-
-    
-
-
-                    
- 
-
-
-
-
-
+    return df_main
 
     
